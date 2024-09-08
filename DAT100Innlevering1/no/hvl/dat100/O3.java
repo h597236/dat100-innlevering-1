430c8a683f5917276fa1904e0e8c325acdb774e3
h597236 <597236@stud.hvl.no>
2024-09-08 21:56:24 +0200

    Levere O3

--- DAT100Innlevering1/no/hvl/dat100/O3.java
+++ DAT100Innlevering1/no/hvl/dat100/O3.java
@@ -1,4 +1,27 @@
 package no.hvl.dat100;
 
+import java.util.Scanner;
+
 public class O3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Skriv inn heiltal større enn 0: ");
+        int n = scanner.nextInt();
+
+        while (n <= 0) {
+            System.out.print("Ugyldig tal. Skriv inn heiltall større enn 0: ");
+            n = scanner.nextInt();
+        }
+
+        long fakultet = 1;
+        int i = 1;
+        while (i <= n) {
+            fakultet *= i;
+            i++;
+        }
+
+        System.out.println("Fakultet av " + n + " er: " + fakultet);
+    }
 }
